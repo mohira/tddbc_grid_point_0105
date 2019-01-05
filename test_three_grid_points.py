723288f390bc5dc74aa12ed62948ad8c1ca8757c
mohira <shukatsudaison@gmail.com>
2019-01-05 19:59:33 +0900

    Move

--- test_three_grid_points.py
+++ test_three_grid_points.py
@@ -1,33 +1,7 @@
 import unittest
-from dataclasses import dataclass
 
 from grid_point import GridPoint
-from two_grid_points import TwoGridPoints
-
-
-@dataclass
-class ThreeGridPoints:
-    p1: GridPoint
-    p2: GridPoint
-    p3: GridPoint
-
-    def contains(self, p: GridPoint) -> bool:
-        return self.p1.has_same_coordinates_with(p) \
-               or self.p2.has_same_coordinates_with(p) \
-               or self.p3.has_same_coordinates_with(p)
-
-    def is_connected(self) -> bool:
-        s1 = TwoGridPoints(self.p1, self.p2)
-        s2 = TwoGridPoints(self.p2, self.p3)
-        s3 = TwoGridPoints(self.p3, self.p1)
-
-        # 3点が構成する 3つの 2点格子点集合 のうち ある2つの2点格子点集合が連結である
-        return (s1.is_connected() and s2.is_connected()) or \
-               (s2.is_connected() and s3.is_connected()) or \
-               (s3.is_connected() and s1.is_connected())
-
-    def count(self) -> int:
-        return 3
+from three_grid_points import ThreeGridPoints
 
 
 class TestThreeGridPoints(unittest.TestCase):
@@ -39,7 +13,7 @@ class TestThreeGridPoints(unittest.TestCase):
 
             self.assertTrue(ThreeGridPoints(p00, p11, p22).contains(p22))
 
-        with self.subTest("含む"):
+        with self.subTest("含まない"):
             p00 = GridPoint(0, 0)
             p11 = GridPoint(1, 1)
             p22 = GridPoint(2, 2)
---
+++ three_grid_points.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+from grid_point import GridPoint
+from two_grid_points import TwoGridPoints
+
+
+@dataclass
+class ThreeGridPoints:
+    p1: GridPoint
+    p2: GridPoint
+    p3: GridPoint
+
+    def contains(self, p: GridPoint) -> bool:
+        return self.p1.has_same_coordinates_with(p) \
+               or self.p2.has_same_coordinates_with(p) \
+               or self.p3.has_same_coordinates_with(p)
+
+    def is_connected(self) -> bool:
+        s1 = TwoGridPoints(self.p1, self.p2)
+        s2 = TwoGridPoints(self.p2, self.p3)
+        s3 = TwoGridPoints(self.p3, self.p1)
+
+        # 3点が構成する 3つの 2点格子点集合 のうち ある2つの2点格子点集合が連結である
+        return (s1.is_connected() and s2.is_connected()) or \
+               (s2.is_connected() and s3.is_connected()) or \
+               (s3.is_connected() and s1.is_connected())
+
+    def count(self) -> int:
+        return 3
